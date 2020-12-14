dc157b408d4212a1d784569361a5e0cba6238c44
Geta Bugnar <bugnargeta@gmail.com>
2020-12-13 17:54:37 -0800

    added constructor

--- src/main/java/org/fasttrackit/App.java
+++ src/main/java/org/fasttrackit/App.java
@@ -7,7 +7,11 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Welcome to the Racing Game !");
 
-        Car car = new Car();
+        Engine carEngine = new Engine();
+        carEngine.manufacturer = "Renault";
+        carEngine.capacity = 1.5;
+
+        Car car = new Car(carEngine);
         car.name = "Dacia";
         car.color = "red";
         car.mileage = 9.8;
@@ -16,17 +20,11 @@ public class App {
         car.running = false;
         car.doorCount = 4;
 
-        Engine carEngine = new Engine();
-        carEngine.manufacturer = "Renault";
-        carEngine.capacity = 1.5;
-
-        car.engine = carEngine;
 
         System.out.println("Engine detail " + car.engine.manufacturer + ".");
 
         double accelerateDistance = car.accelerate(60, 1);
         System.out.println("Acceleration distance " + accelerateDistance);
-
 
 
     }
