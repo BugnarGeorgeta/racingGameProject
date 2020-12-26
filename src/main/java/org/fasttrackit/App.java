5bd1f74c86d4fe869acc733c3732d19b5e1c2b13
Geta Bugnar <bugnargeta@gmail.com>
2020-12-25 17:13:01 -0800

    arrays, for loop

--- src/main/java/org/fasttrackit/App.java
+++ src/main/java/org/fasttrackit/App.java
@@ -10,40 +10,40 @@ public class App {
         Game game = new Game();
         game.start();
 
-        Joker joker = new Joker();
-        joker.setName("VW");
-        joker.setFuelLevel(80);
-        joker.accelerate(60,1);
-
-        System.out.println("Joker 's traveled distance " + joker.getTraveledDistance());
-
-        Engine carEngine = new Engine();
-        carEngine.setManufacturer("Renault");
-        carEngine.setCapacity(1.5);
-
-
-        Car car = new Car(carEngine);
-        car.setName("Dacia");
-        car.setColor("red");
-        car.setMileage(9.8);
-        car.setFuelLevel(60);
-        car.setMaxSpeed(200);
-        car.setRunning(false);
-        car.setDoorCount(4);
-
-        Vehicle.totalCount = 2;
-
-        Car car2 = new Car();
-        car2.setName("VW");
-
-
-
-
-        System.out.println("Engine detail " + car.getEngine().getManufacturer() + ".");
-
-        double accelerateDistance = car.accelerate(110, 1);
-        System.out.println("Acceleration distance " + accelerateDistance);
-
-
-    }
+//        Joker joker = new Joker();
+//        joker.setName("VW");
+//        joker.setFuelLevel(80);
+//        joker.accelerate(60,1);
+//
+//        System.out.println("Joker 's traveled distance " + joker.getTraveledDistance());
+//
+//        Engine carEngine = new Engine();
+//        carEngine.setManufacturer("Renault");
+//        carEngine.setCapacity(1.5);
+//
+//
+//        Car car = new Car(carEngine);
+//        car.setName("Dacia");
+//        car.setColor("red");
+//        car.setMileage(9.8);
+//        car.setFuelLevel(60);
+//        car.setMaxSpeed(200);
+//        car.setRunning(false);
+//        car.setDoorCount(4);
+//
+//        Vehicle.totalCount = 2;
+//
+//        Car car2 = new Car();
+//        car2.setName("VW");
+//
+//
+//
+//
+//        System.out.println("Engine detail " + car.getEngine().getManufacturer() + ".");
+//
+//        double accelerateDistance = car.accelerate(110, 1);
+//        System.out.println("Acceleration distance " + accelerateDistance);
+//
+//
+   }
 }
--- src/main/java/org/fasttrackit/Game.java
+++ src/main/java/org/fasttrackit/Game.java
@@ -4,7 +4,12 @@ import java.util.Scanner;
 
 public class Game {
 
+    private Track[] tracks = new Track[3];
+
     public void start() {
+
+        initializeTracks();
+        displayTraks();
         System.out.println("Starting game.");
         int numberOfPlayers = getNumberOfPlayers();
         System.out.println("In this game you have " + numberOfPlayers + " players.");
@@ -17,6 +22,32 @@ public class Game {
         Scanner scanner = new Scanner(System.in);
         return scanner.nextInt();
 
+    }
+
+    private void initializeTracks() {
+
+        Track track1 = new Track();
+        track1.setName("SilverStone");
+        track1.setLength(4.5);
+
+        tracks[0] = track1;
+
+        Track track2 = new Track();
+        track2.setName("Transilvania");
+        track2.setLength(4.7);
+
+        tracks[1] = track2;
+    }
+
+    private void displayTraks() {
+        System.out.println("Available tracks :");
+
+        for (int i = 0; i < tracks.length; i++) {
+            if (tracks[i] != null) {
+                System.out.println((i + 1) + "." + tracks[i].getName()
+                        + " - " + tracks[i].getLength() + "km.");
+            }
+        }
     }
 
     private String getVehicleNameFromUser() {
